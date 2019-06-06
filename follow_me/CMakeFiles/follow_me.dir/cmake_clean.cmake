24066ee969651c5200bddfe6c13e466b76bf9f33
Yinon Douchan <yinondou@gmail.com>
2019-06-06 09:50:09 +0300

    first commit in robotaba repo

---
+++ follow_me/CMakeFiles/follow_me.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/follow_me.dir/arduino_serial_comm.cpp.o"
+  "CMakeFiles/follow_me.dir/follow_me.cpp.o"
+  "CMakeFiles/follow_me.dir/detect_markers.cpp.o"
+  "follow_me.pdb"
+  "follow_me"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/follow_me.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
